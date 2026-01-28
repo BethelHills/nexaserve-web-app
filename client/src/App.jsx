4757358c9cb8f71ff2431d720aec85cb95ce768a
BethelHills <bethelhillary@gmail.com>
2026-01-28 12:12:18 -0800

    Wrap public pages with navbar

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,4 +1,5 @@
 import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
+import Navbar from "./components/layout/Navbar";
 import Services from "./pages/public/Services";
 import ServiceDetails from "./pages/public/ServiceDetails";
 import ManageServices from "./pages/admin/ManageServices";
@@ -6,12 +7,14 @@ import ManageServices from "./pages/admin/ManageServices";
 export default function App() {
   return (
     <BrowserRouter>
+      <Navbar />
+
       <Routes>
+        <Route path="/" element={<Navigate to="/services" />} />
         <Route path="/services" element={<Services />} />
         <Route path="/services/:id" element={<ServiceDetails />} />
 
         <Route path="/admin/services" element={<ManageServices />} />
-        <Route path="/" element={<Navigate to="/services" />} />
       </Routes>
     </BrowserRouter>
   );
