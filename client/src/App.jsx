94bb846f09fafad6b93885a4a606c68e63fd458c
BethelHills <bethelhillary@gmail.com>
2026-01-28 08:29:00 -0800

    Wire routes in App component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,20 +1,18 @@
-import { Navigate, Route, Routes } from "react-router-dom";
+import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
 import Services from "./pages/public/Services";
 import ServiceDetails from "./pages/public/ServiceDetails";
 import ManageServices from "./pages/admin/ManageServices";
 
 export default function App() {
   return (
-    <Routes>
-      <Route path="/" element={<Navigate to="/services" replace />} />
-      <Route path="/services" element={<Services />} />
-      <Route path="/services/:id" element={<ServiceDetails />} />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/services" element={<Services />} />
+        <Route path="/services/:id" element={<ServiceDetails />} />
 
-      <Route path="/admin" element={<Navigate to="/admin/services" replace />} />
-      <Route path="/admin/services" element={<ManageServices />} />
-      <Route path="/admin/*" element={<div>Coming soon</div>} />
-
-      <Route path="*" element={<div>Page not found</div>} />
-    </Routes>
+        <Route path="/admin/services" element={<ManageServices />} />
+        <Route path="/" element={<Navigate to="/services" />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
